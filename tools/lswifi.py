2f866c21f947d9e406e2d726b95c7df51979471b
AdamC19 <cordingleyadam19+github@gmail.com>
2020-11-04 01:03:04 -0500

    Fixes to lswifi.py

--- tools/lswifi.py
+++ tools/lswifi.py
@@ -2,14 +2,15 @@
 
 import os
 
-stream = os.popen("sudo iwlist wlan0 scan") # will work without password if install succeeded in modifying the sudoers file
+stream = os.popen("sudo /sbin/iwlist wlan0 scan") # will work without password if install succeeded in modifying the sudoers file
 
 scan = stream.read()
 
 ssid_list = []
 
-for line in scan:
+for line in scan.split('\n'):
+    print(line)
     if 'ESSID' in line:
-        ssid_list.append(line.strip().split(':')[1])
+        ssid_list.append(line.strip().split(':')[1].strip('"'))
 
 print(','.join(ssid_list))
